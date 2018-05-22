39789f14f4acd939dcfc193fcad71610c6031d53
Eamonn Laffey <eamonn.laffey@gmail.com>
2018-05-22 12:09:57 +1000

    Add collection view cell

---
+++ FactsApp/FactCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FactCollectionViewCell.swift
+//  FactsApp
+//
+//  Created by Eamonn Laffey on 22/5/18.
+//  Copyright © 2018 Eamonn Laffey. All rights reserved.
+//
+
+import UIKit
+
+class FactCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var factImage: UIImageView!
+    @IBOutlet var factTitle: UILabel!
+}
