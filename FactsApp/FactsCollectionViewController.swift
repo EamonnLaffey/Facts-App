c5ce5269e67fee5234955fcb8489d2aef9cdf530
Eamonn Laffey <eamonn.laffey@gmail.com>
2018-05-22 11:57:07 +1000

    Fetch and parse json

--- FactsApp/FactsCollectionViewController.swift
+++ FactsApp/FactsCollectionViewController.swift
@@ -8,7 +8,51 @@
 
 import UIKit
 
+struct Facts: Decodable {
+    let title: String
+    let rows: [Row]
+}
+
+struct Row: Decodable {
+    let title: String?
+    let description: String?
+    let imageHref: URL?
+}
+
 class FactsCollectionViewController: UICollectionViewController {
+    var facts: Facts?
+
+    func fetchData() {
+        let url = URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json")!
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if error != nil {
+                print(error!.localizedDescription)
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            // I convert the data here to a string and back again because
+            // JSONDecoder expects it to be encoded utf8 but the endpoint
+            // return ascii
+            guard let dataString = String(data: data, encoding: .ascii) else {
+                return
+            }
+            guard let dataConverted = dataString.data(using: .utf8) else {
+                return
+            }
+            print("Data ", dataString)
+            do {
+                self.facts = try JSONDecoder().decode(Facts.self, from: dataConverted)
+                print(self.facts!.title)
+                DispatchQueue.main.async {
+                    self.collectionView?.reloadData()
+                }
+            } catch let jsonError {
+                print("Json Error", jsonError)
+            }
+        }.resume()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +63,4 @@ class FactsCollectionViewController: UICollectionViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
