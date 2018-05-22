a1c5b0d31b4382c055f597315ad6921aed151bdb
Eamonn Laffey <eamonn.laffey@gmail.com>
2018-05-22 12:23:47 +1000

    Load and display images

--- FactsApp/FactsCollectionViewController.swift
+++ FactsApp/FactsCollectionViewController.swift
@@ -19,8 +19,59 @@ struct Row: Decodable {
     let imageHref: URL?
 }
 
+struct Image {
+    var fetched = false
+    var image: UIImage?
+}
+
 class FactsCollectionViewController: UICollectionViewController {
     var facts: Facts?
+    var images = [Int: Image]()
+
+    override func collectionView(_: UICollectionView, numberOfItemsInSection _: Int) -> Int {
+        if let facts = self.facts {
+            return facts.rows.count
+        } else {
+            return 0
+        }
+    }
+
+    func downloadImageFrom(url: URL, at indexPath: IndexPath) {
+        let index = indexPath.row
+        let keyDoesntExists = images[index] == nil
+        if keyDoesntExists || images[index]?.fetched == false {
+            var image = Image()
+            image.fetched = true
+            images[index] = image
+
+            URLSession.shared.dataTask(with: url) { data, _, _ in
+                DispatchQueue.main.async {
+                    if let data = data {
+                        self.images[index]?.image = UIImage(data: data)
+                        self.collectionView?.reloadItems(at: [indexPath])
+                    }
+                }
+            }.resume()
+        }
+    }
+
+    override func collectionView(_: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView?.dequeueReusableCell(withReuseIdentifier: "factCollectionViewCell",
+                                                       for: indexPath) as! FactCollectionViewCell
+        let index = indexPath.row
+        if let row = facts?.rows[index] {
+            cell.factTitle.text = row.title
+            if let imageHref = row.imageHref {
+                downloadImageFrom(url: imageHref, at: indexPath)
+                if images[index] != nil {
+                    cell.factImage.image = images[index]?.image
+                }
+            } else {
+                cell.factImage.image = nil
+            }
+        }
+        return cell
+    }
 
     func fetchData() {
         let url = URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json")!
@@ -56,11 +107,11 @@ class FactsCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        fetchData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
